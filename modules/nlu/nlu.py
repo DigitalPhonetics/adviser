67c30b853a4572ab082189a33fe8b2909430d8c1
Maximilian Schmidt <maximilian.schmidt@ims.uni-stuttgart.de>
2019-08-19 10:26:11 +0200

    Fix to root path finding in nlu

--- modules/nlu/nlu.py
+++ modules/nlu/nlu.py
@@ -33,7 +33,7 @@ from utils.common import Language
 
 
 def get_root_dir():
-    return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class HandcraftedNLU(Module):
