b019939623f00fe305a8fe9da16beabbfff352b0
Dirk Väth <dirk.vaeth@uni-stuttgart.de>
2020-07-21 12:28:39 +0200

    changed model download source to fixed domain name

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,4 @@
-wget http://193.196.53.250/models/adviser_models.zip
+wget http://adviserresources.ims.uni-stuttgart.de/models/adviser_models.zip
 unzip adviser_models.zip
 mv adviser_models adviser/resources/models
 rm adviser_models.zip
