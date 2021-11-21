3804b770c6a310328c89bf2e7c2eadd09fe3a480
Dirk Vaeth <dirk.vaeth@ims.uni-stuttgart.de>
2021-11-21 17:31:15 +0100

    added support for topic prefix matching

--- adviser/services/backend/router.py
+++ adviser/services/backend/router.py
@@ -117,7 +117,7 @@ class Router:
 	def __init__(self, url: str = 'localhost', port: int = 44122):
 		self.sessions = {}
 		self.topic_to_subscription_id = {}
-		self.topic_subscribers = {}
+		self.topic_subscribers = {} # Todo: create a domain tree later and traverse sub-tree of topic
 		self.url = url
 		self.port = port
 
@@ -194,10 +194,12 @@ class Router:
 		# forward es event to each subscriber
 		publication_id = random.randint(1, 2**53)
 		if topic in self.topic_subscribers:
-			for sub_socket in self.topic_subscribers[topic]:
-				self.logger.info(f"publishing to {topic}, {sub_socket}")
-				msg = f"[{MessageCode.EVENT.value},{self.topic_to_subscription_id[topic]},{publication_id}" + ",{}" + serialized_msg[start_idx:]
-				asyncio.create_task(sub_socket.send(msg))
+			for subscribe_topic in self.topic_subscribers:
+				if subscribe_topic.startswith(topic):
+					for sub_socket in self.topic_subscribers[subscribe_topic]:
+						self.logger.info(f"publishing to {topic}, {self.topic_subscribers[subscribe_topic]}")
+						msg = f"[{MessageCode.EVENT.value},{self.topic_to_subscription_id[topic]},{publication_id}" + ",{}" + serialized_msg[start_idx:]
+						asyncio.create_task(sub_socket.send(msg))
 
 
 	async def msg_loop(self, websocket: websockets.WebSocketServerProtocol, path: str, clients_to_connect: list):
